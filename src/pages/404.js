9fe32ace6f4cb267f0f2098c5b6cbeb5e0efe26b
“Moggach” <acunnane13@gmail.com>
2020-10-09 19:04:29 +0100

    fixed 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,49 +1,54 @@
 import React from "react"
-import "../components/bloglist.css"
+import "./globalstyles.css"
+import "./404.css"
 import avatar from "../images/avatar.jpg"
-import github from "../images/github-brands.svg"
-import twitter from "../images/twitter-brands.svg"
-import linkedin from "../images/linkedin-brands.svg"
+
 import { Link } from "gatsby"
+import Head from "../components/head"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faTwitter,
+  faLinkedin,
+  faGithub,
+} from '@fortawesome/free-brands-svg-icons'
 
 const NotFound = () => {
    return (
+<div className="container">
+        <Head title="About"/>
+      <div className="sidebar">
+     
+      <img className="avatar" src={avatar}/>
+        <div className="about">
+          <p className="title">Anna Cunnane</p>
+          <p>Hi! I'm Anna &#128075; I work in publishing and this is my tech blog &#128105;&#8205;&#128187;</p>
+          
+        </div>
 
-    <div className="container">
-    <div className="sidebar">
-    
-    <img className="avatar" src={avatar}/>
-      <div className="about">
-        <p className="title">Anna Cunnane</p>
-        <p>Pellentesque odio nisi, euismod in, pharetra a
-          ultricies in, diam. Sed arcu. </p>
-      </div>
+ 
+  <ul>
+  <li><Link to="/">Blog</Link></li>
+   <li className="active"><Link to="/about">About Me</Link></li>
+  </ul>
 
-
-<ul>
- <li><Link to="/">Articles</Link></li>
- <li><Link to="/about">About Me</Link></li>
-</ul>
-
-      <a href="https://twitter.com/MollyBloom1989" target="_blank">
-        <img className="social-icons" src={twitter} alt="" />
-      </a>
-      <a href="https://www.linkedin.com/in/annacunnane/" target="_blank">
-        <img className="social-icons" src={linkedin} alt="" />
-      </a>
-      <a href="https://github.com/Moggach" target="_blank">
-        <img className="social-icons" src={github} alt="" />
-      </a>
-      <div className="copyright">@ All Rights Reserved</div>
-      </div>
-
-      <div>
-
-     <div>Page not found</div>
-     <Link to="/">Head home...</Link>
+  <a href="https://twitter.com/MollyBloom1989" target="_blank" rel="noreferrer">
+        <FontAwesomeIcon className="icons" icon={faTwitter} />
+        </a>
+        <a href="https://www.linkedin.com/in/annacunnane/" target="_blank" rel="noreferrer">
+        <FontAwesomeIcon className="icons" icon={faLinkedin}  />
+        </a>
+        <a href="https://github.com/Moggach" target="_blank" rel="noreferrer">
+        <FontAwesomeIcon className="icons" icon={faGithub}  />
+        </a>
+        <div className="copyright">@ All Rights Reserved</div>
+        </div>
+      <div className="not-found">
+     <div><h3>Page not found</h3>
+     <p><Link to="/">Head home...</Link></p></div>
+     </div>
 </div>
 
-</div>
+
 
    )
 
