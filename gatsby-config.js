5b0856e6df0a8d59877da1e015605a5c572db136
“Moggach” <acunnane13@gmail.com>
2020-10-18 14:46:14 +0100

    fixed config issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,43 +5,64 @@ module.exports = {
     author: `Anna Cunnane`,
   },
   
-  plugins: [
 
-    {
-        resolve: `gatsby-plugin-gtag`,
-        options: {
-          
-          trackingId: `UA-180669945-1',
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        
+        'gatsby-plugin-sass',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'src',
+                path: `${__dirname}/src/`
+            }
         },
-      },
-
-      {
-        resolve: 'gatsby-source-filesystem',
-        options: {
-            name: 'src',
-            path: `${__dirname}/src/`
-        }
-    }
-
-    'gatsby-plugin-react-helmet',
-   
-    
-    'gatsby-plugin-sharp',
-    {
-        resolve: 'gatsby-transformer-remark',
-        options: {
-            plugins: [
-                'gatsby-remark-relative-images',
-                {
-                    resolve: 'gatsby-remark-images',
-                    options: {
-                        maxWidth: 600,
-                        linkImagesToOriginal: false
-      
+        'gatsby-plugin-sharp',
+        {
+            resolve: 'gatsby-transformer-remark',
+            options: {
+                plugins: [
+                    'gatsby-remark-relative-images',
+                    {
+                        resolve: 'gatsby-remark-images',
+                        options: {
+                            maxWidth: 750,
+                            linkImagesToOriginal: false
+                        }
                     }
-                }
-            ]
+                ]
+            }
         }
-    }
-]
+
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+              // The property ID; the tracking code won't be generated without it
+              trackingId: "UA-180669945-1",
+              // Defines where to place the tracking script - `true` in the head and `false` in the body
+              head: false,
+              // Setting this parameter is optional
+              anonymize: true,
+              // Setting this parameter is also optional
+              respectDNT: true,
+              // Avoids sending pageview hits from custom paths
+              exclude: ["/preview/**", "/do-not-track/me/too/"],
+              // Delays sending pageview hits on route update (in milliseconds)
+              pageTransitionDelay: 0,
+              // Enables Google Optimize using your container Id
+              optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+              // Enables Google Optimize Experiment ID
+              experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+              // Set Variation ID. 0 for original 1,2,3....
+              variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+              // Defers execution of google analytics script after page load
+              defer: false,
+              // Any additional optional fields
+              sampleRate: 5,
+              siteSpeedSampleRate: 10,
+              cookieDomain: "example.com",
+            },
+          },
+
+    ]
 }
