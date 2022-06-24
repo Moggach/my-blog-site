29061786ec434ca2af98dec678869f926b19d2a1
Anna Cunnane <acunnane13@gmail.com>
2022-06-24 19:08:15 +0100

    add bloglist back into index page

--- gatsby-node.js
+++ gatsby-node.js
@@ -50,7 +50,7 @@ exports.createPages = ({ graphql, actions }) => {
         })
 
         // Create blog post list pages
-        const postsPerPage = 8
+        const postsPerPage = 6
         const numPages = Math.ceil(posts.length / postsPerPage)
 
         Array.from({ length: numPages }).forEach((_, i) => {
