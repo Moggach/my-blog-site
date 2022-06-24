4eb8f885382cc28230db1857a840706aae9158e8
Anna Cunnane <acunnane13@gmail.com>
2022-06-24 18:59:28 +0100

    update gatsby version;

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,11 @@ import React from 'react'
 import './globalstyles.css'
 import Head from '../components/head'
 import Layout from '../components/layout'
-import BlogList from '../templates/blog-list'
 
 const Index = function () {
     return (
         <Layout>
             <Head title="Blog" />
-            <BlogList />
         </Layout>
     )
 }
